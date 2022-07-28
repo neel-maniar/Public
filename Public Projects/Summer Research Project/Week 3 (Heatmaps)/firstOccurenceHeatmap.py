b892cf0069870c8fe5159200e723d6a0139d6f63
neel-maniar <neelmaniar02@gmail.com>
2022-07-28 15:16:57 +0100

    ye

--- Public Projects/Summer Research Project/Week 3 (Heatmaps)/firstOccurenceHeatmap.py
+++ Public Projects/Summer Research Project/Week 3 (Heatmaps)/firstOccurenceHeatmap.py
@@ -3,12 +3,13 @@ from time import time
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+import seaborn as sns; sns.set_theme()
 import sys
 sys.path.insert(0, 'C:/Users/Neel/My Python Coding/Public/Public Projects/Summer Research Project')
 from commonFunctions import *
+np.seterr(divide='ignore', invalid='ignore')
 
 # Measure how long the program takes to run
-start_time = time()
 
 
 # Parameters
@@ -19,18 +20,17 @@ version=3
 
 for div in divList:
     dataList=np.array(getDataList(plating,culture,div) )
-    dataList=np.array([[1,1],[2,2],[3,3],[4,1],[5,1]])
+    channelList=dataList[:,1].astype(int)
     for masterChannel in range(60):
-        print(f"{masterChannel}/59")
-        masterIndices=np.array((dataList[:,1]==masterChannel).nonzero()[0]).T
-        totDataList=np.zeros(64)
-        pointsList=np.zeros(64)
-        unique, counts = np.unique(dataList[:,1], return_counts=True)
+        masterIndices=np.array((channelList==masterChannel).nonzero()[0]).T
+        totDataList=np.zeros(64).astype(int)
+        pointsList=np.zeros(64).astype(int)
+        unique, counts = np.unique(channelList, return_counts=True)
         for i,val in enumerate(unique):
             totDataList[val]=counts[i]
         for index in masterIndices:
             if index+1<len(dataList):
-                pointsList[dataList[index+1][1]]+=1
+                pointsList[int(dataList[index+1][1])]+=1
         propList=(pointsList/totDataList)
 
         totDataMatrix=np.zeros((8,8)).astype(int)
@@ -41,10 +41,11 @@ for div in divList:
                 totDataMatrix[r][c]=totDataList[coordToNum(r,c)]
         mask=np.zeros_like(propMatrix)
         mask[np.isnan(propMatrix)]=True
-        print(mask)
         # Plot Heatmap
-        import seaborn as sns; sns.set_theme()
+        fig, ax = plt.subplots()
+        start_time = time()
         ax = sns.heatmap(propMatrix,annot=totDataMatrix,fmt='',mask=mask,robust=True)
+        print(f"{masterChannel}/59",time()-start_time)
         ax.set_title(f"",wrap=True)
         position=(numToCoord(masterChannel)[1],numToCoord(masterChannel)[0])
         ax.add_patch(Rectangle(position, 1, 1, fill=False, edgecolor='blue', lw=3))
--- Public Projects/Summer Research Project/Week 3 (Heatmaps)/relTimeIntHeatmap.py
+++ Public Projects/Summer Research Project/Week 3 (Heatmaps)/relTimeIntHeatmap.py
@@ -1,4 +1,5 @@
 import re
+import seaborn as sns; sns.set_theme()
 import bz2
 import os
 from time import time
@@ -8,56 +9,29 @@ import matplotlib.pyplot as plt
 import statistics
 from matplotlib.patches import Rectangle
 import copy
+import sys
+sys.path.insert(0, 'C:/Users/Neel/My Python Coding/Public/Public Projects/Summer Research Project')
+from commonFunctions import *
+np.seterr(divide='ignore', invalid='ignore')
 
 # Measure how long the program takes to run
-start_time = time()
 
 # Parameters
 plating=3
 culture=4
-divList=[31]
+divList=[20,31]
+version=2
 
 for div in divList:
-    ## List of URLs for a particular day
-    listsURL="https://neurodatasharing.bme.gatech.edu/development-data/html/wget/daily.spont.dense.text."+str(plating)+"."+str(culture)+".0.list"
-    # Get it from the internet
-    response = requests.get(listsURL)
-    # Put it in a file
-    dirname = os.path.dirname(__file__)
-    path = dirname+"/listOfURLs"
-    open(path, "wb").write(response.content)
-    listOfURLs = [line.rstrip() for line in open(path)]
-    listOfDivs=[]
-    # Find the divs for which data is available, by regex
-    for line in listOfURLs:
-        match = re.search('(\d+)(?=\s*\.spk\.txt\.bz2)', line)
-        if match:
-            listOfDivs.append(int(match.group(1)))
-
-    # Which div?
-    try:
-        listIndex=listOfDivs.index(div)
-    except:
-        print("There was no data recorded for plating "+str(plating)+", culture "+str(culture)+" on div "+str(div))
-        quit()
-
-    URL=listOfURLs[listIndex]
-    ## Import Data from URL
-    response = requests.get(URL)
-    dirname = os.path.dirname(__file__)
-    path = dirname+"\\testbz2"
-    newPath = path.replace(os.sep, '/')
-    open(newPath, "wb").write(response.content)
-
-    ## Process data by reading it off as a string and converting to list
-    bz_file = bz2.BZ2File(newPath)
-    data = bz_file.read().decode('ascii')
-    dataList = [[float(line.split()[0]),int(line.split()[1])] for line in data.splitlines()]
+    dataList=getDataList(plating,culture,div)
     for masterChannel in range(60):
         print(f"{masterChannel}/59")
         timeListMasterChannel=[[index,row[0]] for index,row in enumerate(dataList) if row[1] == masterChannel]
         timeIntervals=[[] for i in range(60)]
         checkChannel=[i for i in range(60)]
+
+        start_time = time()
+
         for index,masterTime in timeListMasterChannel:
             tic=time()
             AppendedYet=[False for i in range(60)]
@@ -77,11 +51,9 @@ for div in divList:
                 if AppendedYet[channel]==False:
                     checkChannel.remove(channel)
             # print(f"{index}/{timeListMasterChannel[-1]} took ----- {time()-tic} ----- seconds to run")
-        
-        def numToCoord(num):
-            rows=[6, 7, 5, 4, 7, 6, 7, 5, 6, 6, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 1, 1, 2, 0, 1, 0, 3, 2, 0, 1, 1, 0, 2, 3, 0, 1, 0, 2, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 5, 7, 6, 7, 4, 5, 7, 6, 0, 0, 7, 7, -1]
-            cols=[3, 3, 3, 3, 2, 2, 1, 2, 1, 0, 1, 0, 2, 1, 0, 0, 1, 2, 0, 1, 0, 1, 2, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 6, 5, 6, 7, 6, 7, 5, 6, 7, 7, 6, 5, 7, 6, 7, 6, 5, 6, 5, 5, 4, 4, 4, 4, 0, 7, 0, 7, -1]
-            return([rows[ num ],cols[ num ]])
+
+        print(f"{os.path.basename(__file__)} took ----- {time()-start_time} ----- seconds to run")
+
         cvListCoord=np.array([[-100.0 for i in range(8)] for j in range(8)])
         for index,value in enumerate(timeIntervals):
             x=numToCoord(index)[1]
@@ -99,8 +71,7 @@ for div in divList:
             if entry[1]==14:
                 count+=1
         # Plot Heatmap
-        import numpy as np; np.random.seed(0)
-        import seaborn as sns; sns.set_theme()
+        fig, ax = plt.subplots()
         cvListArray = np.array(cvListCoord)
         cvListRounded=np.around(cvListArray, decimals=3)
         ax = sns.heatmap(cvListArray,annot=cvListRounded,fmt='',vmin=0,vmax=2,mask=mask,robust=True)
@@ -108,47 +79,6 @@ for div in divList:
         position=(numToCoord(masterChannel)[1],numToCoord(masterChannel)[0])
         ax.add_patch(Rectangle(position, 1, 1, fill=False, edgecolor='blue', lw=3))
         dirname = os.path.dirname(__file__)
-        plt.savefig(f'{dirname}/Heatmaps/Heatmap2_{plating}_{culture}_{div}_{masterChannel}.eps', format='eps')
-        plt.clf()
-#     print(f"Figures produced for div {div}!")
+        plt.savefig(f'{dirname}/Heatmaps/Heatmap{version}_{plating}_{culture}_{div}_{masterChannel}.eps', format='eps')
 
-    # Making a tex file to make the pdf
-    changeMatrix=[[0 for i in range(8)] for j in range(8)]
-
-    for i in range(64):
-        changeMatrix[numToCoord(i)[0]][numToCoord(i)[1]]=i
-
-    dirname = os.path.dirname(__file__)
-    path = f"{dirname}/tex/heatmapTex2_{plating}_{culture}_{div}.tex"
-    file=open(path, "a")
-    file.write(
-    r'''\documentclass{standalone}
-\usepackage{graphicx}
-\begin{document}
-\renewcommand{\arraystretch}{0}
-\setlength{\tabcolsep}{0pt}
-\begin{tabular}{ *8{c} }
-'''
-    )
-
-    for i in range(8):
-        for j in range(8):
-            if j==0 and (i==0 or i==7):
-                file.write(" & ")
-            elif j==7 and not (i==0 or i==7):
-                file.write(f"\includegraphics[width=.18\linewidth,height=.15\linewidth]{{Heatmap2_{plating}_{culture}_{div}_{changeMatrix[i][j]}.eps}} ")
-            elif j==7:
-                file.write(" ")
-            else:
-                file.write(f"\includegraphics[width=.18\linewidth,height=.15\linewidth]{{Heatmap2_{plating}_{culture}_{div}_{changeMatrix[i][j]}.eps}} & ")
-        if i!=7:
-            file.write("\\\\\n")
-
-    file.write(
-        r'''
-\end{tabular}
-\end{document}
-        '''
-    )
-    file.close()
-print(f"{os.path.basename(__file__)} took ----- {time()-start_time} ----- seconds to run")
+    texMaker(plating,culture,div,version)
